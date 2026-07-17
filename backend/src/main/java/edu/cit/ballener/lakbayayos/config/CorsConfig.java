cfa35e8a30367df84e161872728e5bde64e3acd0
LAB22_USER ON WS01 <L22X16W01@LabHCI.net>
2026-07-17 17:38:38 +0800

    Reconfigured url to the deployed site

--- backend/src/main/java/edu/cit/ballener/lakbayayos/config/CorsConfig.java
+++ backend/src/main/java/edu/cit/ballener/lakbayayos/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://lakbayayos-app.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
